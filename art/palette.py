2df62fc0d8f05c1bb37c9e14fc07e8ed4ec5aefd
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 11:02:25 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -1,12 +1,6 @@
-from
+from tealight.art import (color, line, spot, circle, box, image, text, background)
 
-def square(edges, size):
-  angle = 360.0 / edges
-  for i in range(0, edges):
-    move(size)
-    turn(angle)
+from tealight.art import (screen_width, screen_height)
 
-turn(90)
-for y in range(0,8):    
-   square(4,25)
-   move(25)
+def color_palette():
+  box
