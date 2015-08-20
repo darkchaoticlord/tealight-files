67ee503a41ff2d60be35cc746fce920ea6a3997b
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 10:55:48 +0100

    Closing art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -1,1 +1,12 @@
-print "A new file!"
+from
+
+def square(edges, size):
+  angle = 360.0 / edges
+  for i in range(0, edges):
+    move(size)
+    turn(angle)
+
+turn(90)
+for y in range(0,8):    
+   square(4,25)
+   move(25)
