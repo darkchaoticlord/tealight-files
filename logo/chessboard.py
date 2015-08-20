eb806097e4be7ad291769dd0d295602803e9ae2d
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 10:50:47 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -7,12 +7,6 @@ def square(edges, size):
     turn(angle)
 
 
-for x in range(0,8):
-  for y in range(0,8):    
-     square(4,25)
-     move(25)
-  move(-200)
-  turn(90)
-  move(25)
-  turn(-90)
-  
+for y in range(0,8):    
+   square(4,25)
+   move(25)
