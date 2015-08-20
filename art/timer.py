9056bca5267db508baa428c1c87a6e68398a7433
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 15:03:31 +0100

    Running art/timer.py

--- art/timer.py
+++ art/timer.py
@@ -1,1 +1,29 @@
-print "A new file!"
+from tealight.utils import now
+from tealight.art import text,box,color, background, screen_width, screen_height
+
+color("white")
+box(0,0,screen_width,screen_height)
+color("black")
+
+startTime = now()
+i=0
+global points
+points = 300
+
+def doSecond(): 
+  global points
+  points = points-1
+  color("white")
+  box(0,0,100,100)
+  color("black")
+  text(0,0,points)
+
+def handle_frame():
+  global i
+  if (i % 60) == 0:
+    newTime = now() - startTime
+    doSecond()
+  
+  # Do everything else on framechange
+
+  i=i+1
