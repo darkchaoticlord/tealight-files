5137ff7b3d9a0355d14a68aa14908368fedaeb97
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 14:39:49 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -43,9 +43,15 @@ def handle_mousemove(x,y,button):
 
     color(chosen_color)
     line(lastx, lasty, x, y)
+    send({"x1": lastx, "y1": lasty, "x2": x, "y2": y,"color":chosen_color})
     lastx = x
     lasty = y
     
 palette = ["red", "blue", "green", "purple", "yellow", "black", "pink", "grey"] 
 
 colour_palette()
+
+def handle_message(message):
+  color(message["color"])
+  line(message["x1"], message["y1"], message["x2"], message["y2"])
+  
