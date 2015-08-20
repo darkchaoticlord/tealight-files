f927d8b23c54a661a4e22c924471b13412d8159a
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-20 11:42:10 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -3,7 +3,9 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 from tealight.art import (screen_width, screen_height)
 
 def colour_palette():
-  color("red")
-  box(10, 10, 25, 25)
-  
+  for x in range(0,7):
+    color(palette[i])
+    box((i * 25 + 10), (10), 25, 25)
+
+palette = ["red", "blue", "green", "purple", "yellow", "black", "pink", "grey"] 
 colour_palette()
