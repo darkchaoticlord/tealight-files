74b07b1e1233807b68a04f7ee90e678bc95a40a1
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-21 10:04:07 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -167,14 +167,14 @@ def handle_mousedown(x,y,button):
   if inBox(p2cMinX,p2cMaxX,p2cMinY,p2cMaxY,x,y) == "true":
    print "p2 correct"
   
- # if inBox(p2wMinX,p2wMaxX,p2wMinY,p2wMaxY,x,y) == "true":
-    #print "p2 wrong"
+  if inBox(p2wMinX,p2wMaxX,p2wMinY,p2wMaxY,x,y) == "true":
+    print "p2 wrong"
   
- # if inBox(p3cMinX,p3cMaxX,p3cMinY,p3cMaxY,x,y) == "true":
- #   print "p3 correct"
+  if inBox(p3cMinX,p3cMaxX,p3cMinY,p3cMaxY,x,y) == "true":
+    print "p3 correct"
   
- # if inBox(p3wMinX,p3wMaxX,p3wMinY,p3wMaxY,x,y) == "true":
-  #  print "player 3 wrong"
+  if inBox(p3wMinX,p3wMaxX,p3wMinY,p3wMaxY,x,y) == "true":
+    print "player 3 wrong"
    
     
     
