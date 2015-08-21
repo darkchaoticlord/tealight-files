6ad7d965a0118adb6572ac3604704931c72e457e
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-21 09:49:26 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -47,6 +47,9 @@ def handle_mousedown(x,y,button):
     chosen_color = palette[getChosen(10,35,x,y,len(palette))]
   print chosen_color
   
+  if inBox(10,20,10,20,5,5) == "true":
+    print "yes"
+  
   global lastx, lasty
   lastx = x
   lasty = y
