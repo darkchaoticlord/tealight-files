fb869b7ddd108570bcf9928b7c1ce9a8db194fc0
darkchaoticlord <jawadadnan46@yahoo.com>
2015-08-21 10:03:14 +0100

    Running art/palette.py

--- art/palette.py
+++ art/palette.py
@@ -1,6 +1,9 @@
 from tealight.art import (color, line, spot, rectangle, circle, box, image, text, background)
 from tealight.art import (screen_width, screen_height)
 
+global p2cMinX, p2cMaxX, p2cMinY, p2cMaxY, p2wMinX, p2wMaxX, p2wMinY, p2wMaxY, p3cMinX, p3cMaxX, p3cMinY, p3cMaxY, p3wMinX, p3wMaxX, p3wMinY, p3wMaxY
+
+
 stopMinY = 0
 stopMaxY = 0
 stopMinX = 0
